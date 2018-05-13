c279512d3baa796f61d7c09c6c917d593bb2ecef
Hrabal <federicocerchiai@gmail.com>
2018-05-13 12:41:17 +0200

    settings refactor

--- app.py
+++ app.py
@@ -12,16 +12,15 @@ def hello_world(request):
 
 
 def make_wsgi_app(env, **vars):
-    parser = ConfigParser()
-    parser.read(f'{env}.ini')
-    settings = dict(parser.items('app:main', vars=vars))
-    with Configurator(**settings) as config:
+    settings = ConfigParser()
+    settings.read(f'{env}.ini')
+    with Configurator(settings=settings) as config:
         routes_l = [route_cls() for route_cls in routes.__all__]
         for route in routes_l:
             config.add_route(route.name, route.url)
             config.add_view(route.handler, route_name=route.name)
         print('Imported routes: %s' % ', '.join(str(r) for r in routes_l) if routes_l else 'Empty /routes directory.')
-        return config.make_wsgi_app(), parser
+        return config.make_wsgi_app()
 
 
 def zappa_wsgi_app(app, environ):
@@ -31,8 +30,9 @@ def zappa_wsgi_app(app, environ):
 
 if __name__ == '__main__':
     env = 'localhost'
-    app, server_settings = make_wsgi_app(env)
-    server = make_server(server_settings.get('server:main', 'host'),
-                         server_settings.getint('server:main', 'port'),
-                         app)
+    app = make_wsgi_app(env)
+    host = app.registry.settings.get('server:main').get('host')
+    port = app.registry.settings.get('server:main').getint('port')
+    server = make_server(host, port, app)
+    print(f'SERVING ON {host}:{port}')
     server.serve_forever()
